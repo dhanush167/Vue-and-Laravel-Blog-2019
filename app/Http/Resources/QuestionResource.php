eb7b11fd721b3d1e28924c6d1addd91e7ae23d16
kadawatha <bydhanushka@gmail.com>
2019-09-07 12:19:01 +0530

    Question Resource - a

--- app/Http/Resources/QuestionResource.php
+++ app/Http/Resources/QuestionResource.php
@@ -14,6 +14,12 @@ class QuestionResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'title' => $this->title,
+            'path'  => $this->path,
+            'body'  => $this->body,
+            'created_at' => $this->created_at,
+            'user' => $this->user->name
+        ];
     }
 }
