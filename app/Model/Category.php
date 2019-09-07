40b8087bb508616a7afc14a656767675eb9346df
kadawatha <bydhanushka@gmail.com>
2019-09-07 16:13:59 +0530

    Real and Delete Category - b

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    //http://localhost:8000/api/category/dolorem 🏺
+   public function getRouteKeyName() {
+    return 'slug';
+   }
+
 }
