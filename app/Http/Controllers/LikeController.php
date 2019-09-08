053c5a6f603b35a2ccadf284478b786a1fdd8f2f
kadawatha <bydhanushka@gmail.com>
2019-09-08 11:40:42 +0530

    Like and DisLikes - a

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Model\Like;
+use App\Model\Reply;
 use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
 
+  public function likeIt(Reply $reply) {
+   $reply->like()->create([
+       //'user_id' => auth()->id()
+       'user_id' => '1'
+   ]);
+  }
+  public function unLikeIt(Reply $reply) {
+          $reply->like()->where(['user_id',auth()->id()])->first()->delete();
+  }
+
 }
+
--- routes/api.php
+++ routes/api.php
@@ -10,4 +10,4 @@ Route::apiResource('question/{question}/reply', 'ReplyController');
 
 Route::post('/like/{reply}','LikeController@likeIt');
 
-Route::delete('/like/{reply}','LikeController@likeIt');
+Route::delete('/like/{reply}','LikeController@unLikeIt');
