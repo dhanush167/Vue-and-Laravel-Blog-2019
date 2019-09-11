8d7e660a8e5798cb445a1f58ed1ad5c3b422a45b
kadawatha <bydhanushka@gmail.com>
2019-09-11 20:42:40 +0530

    43.Storage Helper - a

--- resources/assets/js/Helpers/AppStorage.js
+++ resources/assets/js/Helpers/AppStorage.js
@@ -6,4 +6,24 @@ class AppStorage {
     storeUser(user){
         localStorage.setItem('user',user);
     }
+    store(user,token) {
+        this.storeToken(token);
+        this.storeUser(user);
+    }
+    clear(){
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+    }
+    getToken() {
+        return localStorage.getItem('token')
+    }
+    getUser() {
+        return localStorage.getItem('user')
+    }
 }
+
+export default AppStorage = new AppStorage()
+
+
+
+
