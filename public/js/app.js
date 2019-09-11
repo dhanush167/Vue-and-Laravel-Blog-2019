704e05a8ab0e03d654dfcf00acf92a1bafad6950
kadawatha <bydhanushka@gmail.com>
2019-09-11 22:41:51 +0530

    45.Store Token and Username

--- public/js/app.js
+++ public/js/app.js
@@ -88882,6 +88882,69 @@ module.exports = function(module) {
 
 /***/ }),
 
+/***/ "./resources/assets/js/Helpers/AppStorage.js":
+/*!***************************************************!*\
+  !*** ./resources/assets/js/Helpers/AppStorage.js ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var AppStorage =
+/*#__PURE__*/
+function () {
+  function AppStorage() {
+    _classCallCheck(this, AppStorage);
+  }
+
+  _createClass(AppStorage, [{
+    key: "storeToken",
+    value: function storeToken(token) {
+      localStorage.setItem('token', token);
+    }
+  }, {
+    key: "storeUser",
+    value: function storeUser(user) {
+      localStorage.setItem('user', user);
+    }
+  }, {
+    key: "store",
+    value: function store(user, token) {
+      this.storeToken(token);
+      this.storeUser(user);
+    }
+  }, {
+    key: "clear",
+    value: function clear() {
+      localStorage.removeItem('token');
+      localStorage.removeItem('user');
+    }
+  }, {
+    key: "getToken",
+    value: function getToken() {
+      return localStorage.getItem('token');
+    }
+  }, {
+    key: "getUser",
+    value: function getUser() {
+      return localStorage.getItem('user');
+    }
+  }]);
+
+  return AppStorage;
+}();
+
+/* harmony default export */ __webpack_exports__["default"] = (AppStorage = new AppStorage());
+
+/***/ }),
+
 /***/ "./resources/assets/js/Helpers/Token.js":
 /*!**********************************************!*\
   !*** ./resources/assets/js/Helpers/Token.js ***!
@@ -88905,8 +88968,8 @@ function () {
   }
 
   _createClass(Token, [{
-    key: "isVaid",
-    value: function isVaid(token) {
+    key: "isValid",
+    value: function isValid(token) {
       var payload = this.payload(token);
 
       if (payload) {
@@ -88919,7 +88982,7 @@ function () {
     key: "payload",
     value: function payload(token) {
       var payload = token.split('.')[1];
-      console.log(this.decode(payload));
+      return this.decode(payload);
     }
   }, {
     key: "decode",
@@ -88945,11 +89008,13 @@ function () {
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _Token__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Token */ "./resources/assets/js/Helpers/Token.js");
+/* harmony import */ var _AppStorage__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./AppStorage */ "./resources/assets/js/Helpers/AppStorage.js");
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
 
 
 
@@ -88963,12 +89028,23 @@ function () {
   _createClass(User, [{
     key: "login",
     value: function login(data) {
+      var _this = this;
+
       axios.post('/api/auth/login', data).then(function (res) {
-        //console.log(res.data);
-        _Token__WEBPACK_IMPORTED_MODULE_0__["default"].payload(res.data.access_token);
+        return _this.responseAfterLogin(res);
       })["catch"](function (error) {
         return console.log(error.response.data);
       });
+    }
+  }, {
+    key: "responseAfterLogin",
+    value: function responseAfterLogin(res) {
+      var access_token = res.data.access_token;
+      var username = res.data.user;
+
+      if (_Token__WEBPACK_IMPORTED_MODULE_0__["default"].isValid(access_token)) {
+        _AppStorage__WEBPACK_IMPORTED_MODULE_1__["default"].store(username, access_token);
+      }
     }
   }]);
 
--- resources/assets/js/Helpers/User.js
+++ resources/assets/js/Helpers/User.js
@@ -1,15 +1,22 @@
-import Token from "./Token";
-
+import Token from './Token'
+import AppStorage from './AppStorage'
 
 class User {
-    login(data) {
+    login(data){
         axios.post('/api/auth/login',data)
-            .then(res => {
-                //console.log(res.data);
-                Token.payload(res.data.access_token)
-            })
+            .then(res => this.responseAfterLogin(res))
             .catch(error => console.log(error.response.data))
     }
+
+    responseAfterLogin(res){
+        const access_token = res.data.access_token
+        const username = res.data.user
+
+        if(Token.isValid(access_token)){
+            AppStorage.store(username,access_token)
+        }
+    }
+
 }
 
 export default User = new User();
