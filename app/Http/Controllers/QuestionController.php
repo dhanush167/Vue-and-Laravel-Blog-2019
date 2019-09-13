42e4b39816fb25fd3fb859dc8a7ba55e49d4666e
kadawatha <bydhanushka@gmail.com>
2019-09-13 19:28:03 +0530

    60.Store Question with User_id and slug

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -41,6 +41,9 @@ class QuestionController extends Controller
     public function store(Request $request)
     {
      //auth()->user()->question()->create($request->all());
+    $request['slug'] = str_slug($request->title);
+     $request['user_id'] = str_slug($request->id);
+     auth()->user()->question()->create($request->all());
      Question::create($request->all());
      return response('Created', Response::HTTP_CREATED);
     }
