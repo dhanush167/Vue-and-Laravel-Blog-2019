86d9c796b4aa551e91aba890ebe476967b39ef48
kadawatha <bydhanushka@gmail.com>
2019-09-19 01:44:58 +0530

    83.Send Notification

--- app/Notifications/NewReplyNotification.php
+++ app/Notifications/NewReplyNotification.php
@@ -11,14 +11,16 @@ class NewReplyNotification extends Notification
 {
     use Queueable;
 
+    public $reply;
+
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Reply $reply)
     {
-        //
+       $this->reply = $reply;
     }
 
     /**
@@ -30,20 +32,6 @@ class NewReplyNotification extends Notification
     public function via($notifiable)
     {
         return ['database'];
-    }
-
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
     }
 
     /**
